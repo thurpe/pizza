8e2a1e1dda8d0f248fbaf8f7c440551d2b8490ab
thurpe <babatope.ifesanya@gmail.com>
2021-10-05 19:12:27 +0100

    Added backend starters

--- client/src/components/Pizza.js
+++ client/src/components/Pizza.js
@@ -1,18 +1,86 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { Modal } from "react-bootstrap";
 export default function Pizza({ pizza }) {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  const [quantity, setQuantity] = useState(1);
+  const [varient, setVarient] = useState("small");
+
   return (
-    <div>
-      <h1>{pizza.name}</h1>
-      <img
-        src={pizza.image}
-        alt=""
-        className="img-fluid"
-        style={{ height: "200px", width: "200px" }}
-      />
-      <div className="flex-container">
-        <div></div>
+    <div
+      style={{ margin: "70px" }}
+      className="shadow p-3 mb-5 bg-white rounded"
+    >
+      <div onClick={handleShow}>
+        <h1>{pizza.name}</h1>
+        <img
+          src={pizza.image}
+          alt=""
+          className="img-fluid"
+          style={{ height: "200px", width: "200px" }}
+        />
       </div>
+      <div className="flex-container">
+        <div className="w-100 m-1">
+          <p>Varients</p>
+          <select
+            className="form-control"
+            value={varient}
+            onChange={(e) => {
+              setVarient(e.target.value);
+            }}
+          >
+            {pizza.varients.map((varient) => {
+              return <option value={varient}>{varient}</option>;
+            })}
+          </select>
+        </div>
+        <div className="w-100 m-1">
+          <p>Quantity</p>
+          <select
+            value={quantity}
+            className="form-control"
+            onChange={(e) => {
+              setQuantity(e.target.value);
+            }}
+          >
+            {[...Array(10).keys()].map((x, i) => {
+              return <option value={i + 1}>{i + 1}</option>;
+            })}
+          </select>
+        </div>
+      </div>
+      <div className="flex-container">
+        <div className="m-1 w-100 mt-1">
+          <h1>Price: $ {pizza.prices[0][varient] * quantity} </h1>
+        </div>
+        <div className="m-1 w-100">
+          <button className="btn btn-danger">ADD TO CART</button>
+        </div>
+      </div>
+      <Modal show={show}>
+        <Modal.Header>
+          <Modal.Title>{pizza.name}</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <img
+            src={pizza.image}
+            style={{ height: "400px" }}
+            alt=""
+            className="img-fluid"
+          />
+          <p>{pizza.description}</p>
+        </Modal.Body>
+
+        <Modal.Footer>
+          <button className="btn btn-danger" onClick={handleClose}>
+            CLOSE
+          </button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
-const app = express();
 
-app.listen(5000, function () {
-  console.log("Server started on port 5000 🔥");
-});
+const app = express();
+app.use(express.json());
+
+const dbConfig = require("./db");
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log("Server is now running on port 5000 🔥"));
